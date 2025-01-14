0b3dfe879ca781bb3863793ec5bcc9bd995c4837
Leon Ramos <nemean.dev@gmail.com>
2025-01-14 21:27:13 +0100

    Create bootstrap_wtf.html macro. Use it for forms in all templates.

--- cdl_admin.py
+++ cdl_admin.py
@@ -6,5 +6,3 @@ from app.models import User, AdminAction
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'orm': orm, 'db': db, 'User': User, 'AdminAction': AdminAction}
-
-#TODO remove h1 from base and change h2 in other templates to h1
