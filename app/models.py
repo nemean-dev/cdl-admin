463767dbdd320b5427c8ac8e0011893142e650b9
Leon Ramos <nemean.dev@gmail.com>
2025-01-14 12:01:16 +0100

    AdminAction model and migration script

--- app/models.py
+++ app/models.py
@@ -13,6 +13,23 @@ class User(db.Model):
     password_hash: orm.Mapped[str] = orm.mapped_column(sa.String(256))
     last_seen: orm.Mapped[Optional[datetime]] = orm.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+    actions: orm.WriteOnlyMapped['AdminAction'] = orm.relationship(
+        back_populates='admin')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.email)
+    
+class AdminAction(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(
+        index= True, 
+        default= lambda: datetime.now(timezone.utc))
+    action: orm.Mapped[str] = orm.mapped_column(sa.String(128))
+    status: orm.Mapped[str] = orm.mapped_column(sa.String(16))
+    user_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey(User.id),
+                                                 index=True)
+    admin: orm.Mapped[User] = orm.relationship(back_populates='actions')
+
+    def __repr__(self):
+        return '<AdminAction {}>'.format(self.action)
+    
