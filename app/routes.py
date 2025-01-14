91227a9a3c80223ab8fe521facf2210a43bcbd8f
Leon Ramos <nemean.dev@gmail.com>
2025-01-14 15:46:17 +0100

    Implement user profile pages with actions log

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,33 @@
-from flask import render_template
+from flask import render_template, abort
 from flask_login import login_required
-from app import app
+import sqlalchemy as sa
+from app import app, db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
     return render_template('index.html')
+
+@app.route('/user/<id>')
+@login_required
+def user(id):
+    user = db.session.get(User, id)
+    if user is None:
+        abort(404)
+
+    actions = [
+        {'action': 'Post products', 'status': 'completed', 'admin': user},
+        {'action': 'Generate report', 'status': 'completed', 'admin': user}
+    ]
+
+    return render_template('user.html', user=user, actions=actions)
+
+@app.route('/settings')
+@login_required
+def user_settings():
+    return 'building...'
 
 @app.route('/captura')
 @login_required
