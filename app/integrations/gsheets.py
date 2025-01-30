642432b3b13fdbd7ba58b39768ca75e804f81634
Leon Ramos <nemean.dev@gmail.com>
2025-01-30 01:53:41 +0100

    Modularize gsheets and prevent app context errors

--- app/integrations/gsheets.py
+++ app/integrations/gsheets.py
@@ -1,12 +1,8 @@
 import pandas as pd
-from flask import current_app
 import gspread
 from google.oauth2.service_account import Credentials
 
-CREDENTIALS = current_app.config['GSHEETS_CREDENTIALS']
-CAPTURA_ID = current_app.config['GSHEETS_CAPTURA_ID']
-
-def connect_to_gsheet(spreadsheet_id, sheet_name, credentials_path=CREDENTIALS) -> gspread.worksheet.Worksheet: 
+def connect_to_gsheet(spreadsheet_id, sheet_name, credentials_path) -> gspread.worksheet.Worksheet: 
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
     
     creds = Credentials.from_service_account_file(credentials_path, scopes=scopes)
@@ -15,12 +11,12 @@ def connect_to_gsheet(spreadsheet_id, sheet_name, credentials_path=CREDENTIALS) 
     ss = client.open_by_key(spreadsheet_id)
     return ss.worksheet(sheet_name)
 
-def get_sheet_data(spreadsheet_id, sheet_name) -> list[dict]:
+def get_sheet_data(spreadsheet_id, sheet_name, credentials_path) -> list[dict]:
     """Returns listt of dicts where keys are column headers (first row)."""
-    sheet = connect_to_gsheet(spreadsheet_id, sheet_name)
+    sheet = connect_to_gsheet(spreadsheet_id, sheet_name, credentials_path)
     data = sheet.get_all_records()
     return data 
 
-def get_sheet_as_dataframe(spreadsheet_id, sheet_name) -> pd.DataFrame:
-    data = get_sheet_data(spreadsheet_id, sheet_name)
+def get_sheet_as_dataframe(spreadsheet_id, sheet_name, credentials_path) -> pd.DataFrame:
+    data = get_sheet_data(spreadsheet_id, sheet_name, credentials_path)
     return pd.DataFrame(data)
