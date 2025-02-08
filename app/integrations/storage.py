cd3460007482b3b41280e6ddf6506b9fecb88df9
Leon Ramos <nemean.dev@gmail.com>
2025-02-08 12:22:25 +0100

    Improve storage service handling of paths/keys

--- app/integrations/storage.py
+++ app/integrations/storage.py
@@ -9,7 +9,7 @@ from typing import Any
 class StorageService:    
     def __init__(self):
         self.use_local_storage = current_app.config.get('USE_LOCAL_STORAGE', False)
-        self.local_storage_path = './local_data'
+        self.data_path = current_app.config['DATA_DIR']
 
         if not self.use_local_storage:
             self.client = boto3.client(
@@ -20,22 +20,28 @@ class StorageService:
             )
             self.bucket_name = current_app.config['AWS_BUCKET_NAME']
 
-    def _get_local_path(self, key: str) -> str:
-        '''Returns the full local file path for a given key.'''
-        return os.path.join(self.local_storage_path, key)
+    def _get_path_or_key(self, key: str) -> str:
+        '''
+        Returns the full local file path or full key for a given key. (starting 
+        from the data directory)'''
+        if key[0] == '/':
+            key = key[1:]
+        return os.path.join(self.data_path, key)
 
     def upload_text(self, key: str, text: str) -> None:
         '''Uploads plain text to storage.'''
+        key = self._get_path_or_key(key)
         if self.use_local_storage:
-            with open(self._get_local_path(key), 'w', encoding='utf-8') as f:
+            with open(key, 'w', encoding='utf-8') as f:
                 f.write(text)
         else:
             self.client.put_object(Bucket=self.bucket_name, Key=key, Body=text)
 
     def download_text(self, key: str) -> str:
         '''Downloads plain text from storage.'''
+        key = self._get_path_or_key(key)
         if self.use_local_storage:
-            with open(self._get_local_path(key), 'r', encoding='utf-8') as f:
+            with open(key, 'r', encoding='utf-8') as f:
                 return f.read()
         else:
             obj = self.client.get_object(Bucket=self.bucket_name, Key=key)
@@ -43,19 +49,23 @@ class StorageService:
 
     def upload_json(self, key: str, data: Any) -> None:
         '''Uploads JSON data to storage.'''
+        key = self._get_path_or_key(key)
         self.upload_text(key, json.dumps(data, indent=4))
 
     def download_json(self, key: str) -> Any:
         '''Downloads JSON data from storage.'''
+        key = self._get_path_or_key(key)
         return json.loads(self.download_text(key))
 
     def upload_csv(self, key: str, df: pd.DataFrame) -> None:
         '''Uploads a Pandas DataFrame as a CSV to storage.'''
+        key = self._get_path_or_key(key)
         csv_buffer = StringIO()
         df.to_csv(csv_buffer, index=False)
         self.upload_text(key, csv_buffer.getvalue())
 
     def download_csv(self, key: str) -> pd.DataFrame:
         '''Downloads a CSV from storage into a Pandas DataFrame.'''
+        key = self._get_path_or_key(key)
         csv_data = self.download_text(key)
         return pd.read_csv(StringIO(csv_data))
--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or '421a8124af2a47529272631abf3d0218'
     
+    USE_LOCAL_STORAGE = os.getenv('USE_LOCAL_STORAGE', 'False').lower() in ['true', '1']
+    basedir = os.path.abspath(os.path.dirname(__file__)) if USE_LOCAL_STORAGE else ''
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     
@@ -33,7 +35,6 @@ class Config:
     ADMINS = os.getenv('MAIL_ADMINS').split(',') if os.getenv('MAIL_ADMINS') else None
 
     # S3 storage
-    USE_LOCAL_STORAGE = os.getenv('USE_LOCAL_STORAGE', False)
     AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
     AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
     AWS_REGION = os.getenv('AWS_REGION')
