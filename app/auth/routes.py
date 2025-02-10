4cf0019daa2f482fca1d51ac51b5f35d6c7bcfdc
Leon Ramos <nemean.dev@gmail.com>
2025-02-10 18:03:33 +0100

    Improved login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,12 @@
 from time import sleep
 from urllib.parse import urlsplit
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import current_user, login_required, login_user, logout_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
 from app.auth import bp
+from app.email import send_email
 from app.auth.email import send_password_reset_email
 from app.auth.forms import LoginForm, RegisterUsersForm, ResetPasswordRequestForm, ResetPasswordForm
 
@@ -20,23 +21,34 @@ def login():
         user = db.session.scalar(
             sa.select(User).where(User.email == form.email.data))
         
-        if user.failed_logins > 100:
-            flash('Han habido demasiados intentos de inicio de sesión erróneos.' 
-                  'Contacta a un administrador si el problema persiste.')
-            redirect(url_for('auth.login'))
-        
-        if user is not None and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            flash('Inicio de sesión exitoso')
-            next_page = request.args.get('next')
-            if not next_page or urlsplit(next_page).netloc != '':
-                next_page = url_for('dashboard.index')
+        if user is not None:
+            if user.failed_logins >= 100:
+                if user.failed_logins == 100:
+                    admins:list = current_app.config.get('ADMINS')
+                    send_email(
+                        subject=f"[{current_app.config['STORE_NAME']}] Demasiados inicios de sesión fallidos",
+                        sender=admins[0], # use this default instead of the other env variable?
+                        recipients=admins.append(user.email),
+                        text_body='Hubieron demasiados intentos fallidos de inicio de sesión.\n\n'
+                            'Por motivos de seguridad, contacta a un administrador para desbloquear tu cuenta.',
+                    )
+                sleep(2)
+                flash('Email o contraseña incorrectos.')
 
-            return redirect(next_page)
+                return redirect(url_for('auth.login'))
+        
+            if user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                flash('Inicio de sesión exitoso')
+                next_page = request.args.get('next')
+                if not next_page or urlsplit(next_page).netloc != '':
+                    next_page = url_for('dashboard.index')
+
+                return redirect(next_page)
         
         else:
             sleep(2)
-            flash('Email o contraseña incorrectos')
+            flash('Email o contraseña incorrectos.')
             return redirect(url_for('auth.login'))
     
     return render_template('auth/login.html', title='Inicio de sesión', form=form)
